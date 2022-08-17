6d0e96b76e22a4347c992cd2c71d9f54f54e88db
kaushikkishore <kaushik@vauld.com>
2022-08-17 16:14:20 +0530

    added selection sort

--- src/algoExpert/easy/sorting/selectionSort.ts
+++ src/algoExpert/easy/sorting/selectionSort.ts
@@ -1,4 +1,35 @@
 export function selectionSort(array: number[]) {
   // Write your code here.
+  let start = 0;
+  let end = array.length;
+
+  while (end > start) {
+    const index = getSmallestNumberIndex(array, start, end);
+    let temp = array[index];
+    array[index] = array[start];
+    array[start] = temp;
+    start++;
+  }
+
   return array;
 }
+
+function getSmallestNumberIndex(
+  array: number[],
+  start: number,
+  end: number
+): number {
+  let smallestIndex = start;
+  let smallestNumber = array[start];
+  while (end > start) {
+    if (smallestNumber > array[start]) {
+      smallestIndex = start;
+      smallestNumber = array[start];
+    }
+    start++;
+  }
+  console.log(`Smallest index ${smallestIndex} number ${array[smallestIndex]}`);
+  return smallestIndex;
+}
+
+console.log(selectionSort([8, 5, 2, 9, 5, 6, 3]));
